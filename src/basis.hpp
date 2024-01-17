65f382e37cadf8b50c6ad29b52d1cc18387970d4
Jon Dahl <jon@dira.no>
2024-01-17 18:46:50 +0900

    Calculate basis states as std::bitsets

--- src/basis.hpp
+++ src/basis.hpp
@@ -2,9 +2,11 @@
 #define BASIS_HPP
 
 #include <vector>
+#include <bitset>
+#include "parameters.hpp"
 #include "data_structures.hpp"
 
 std::vector<std::vector<unsigned short>> calculate_m_basis_states(const Interaction& interaction);
-
+std::vector<std::bitset<n_bits_bitset>> calculate_m_basis_states_bit_representation(const Interaction& interaction);
 
 #endif
