38908f2d13e93514fe749f71ad827590bba3d452
Jon Dahl <jon@dira.no>
2024-01-18 14:47:12 +0900

    Add code for executing program

--- src/basic_solver.cpp
+++ src/basic_solver.cpp
@@ -1,34 +1,74 @@
 #include <iostream>
+#include <algorithm>
+#include <chrono>
+#include <bitset>
 #include "data_structures.hpp"
+#include "parameters.hpp"
 #include "loaders.hpp"
 #include "tools.hpp"
+#include "hamiltonian.hpp"
+#include "../external/cppitertools/combinations.hpp"
+#include "../external/eigen-3.4.0/Eigen/Dense"
+#include "../external/eigen-3.4.0/Eigen/Eigenvalues"
+// #include "../external/boost_1_84_0/boost/container_hash/hash.hpp"
 
 using std::cout;
 using std::endl;
+// using std::chrono::high_resolution_clock;
+// using std::chrono::duration_cast;
+// using std::chrono::milliseconds;
 
 int main()
 {
-    std::string interaction_filename = "../snt/w.snt";
-    const Interaction interaction = load_interaction(interaction_filename);
-    cout << interaction.model_space_protons.orbitals[0].j << endl;
 
-    print(interaction.model_space.orbitals);
-    cout << interaction.tbme_mass_dependence_denominator << endl;
+    // std::bitset<16> state_0;
+    // state_0.set(14);
+    // cout << (not 0) << endl;
+    // cout << "state_0: " << state_0 << endl;
+    // state_0.reset(14);
+    // cout << "state_0: " << state_0 << endl;
 
-    const Key key = {0, 0, 0, 0, 0};
-    cout << interaction.tbme_map.at(key) << endl;
+    // double a = 98.5;
+    // cout << a << endl;
+    // cout << a*state_0.test(14) << endl;
 
-    // Key key = {i0, i1, i2, i3, j};
-    // tbme_keys.push_back(key);
-    // tbme_map[key] = tbme;
+    // cout << res << endl;
+    // state.set(3);
+    // if (state.test(4)) cout << "Sub-state 5 is occupied." << endl;
+    // state.reset(4);
+    // state.flip(9);
 
-    // print("n_proton_orbitals", interaction.model_space_protons.n_orbitals);
-    // print("n_neutron_orbitals", interaction.model_space_neutrons.n_orbitals);
+    // std::hash<std::bitset<16>> hasher;
+    // size_t hash_value = hasher(state_0);
+
+    // cout << hash_value << endl;
+
+    // return 0;
+
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> H;
+
+    std::vector<int> timing;
+    for (int i = 0; i < 10; i++)
+    {
+        auto start = timer();
+        unsigned short n_valence_protons = 2;
+        unsigned short n_valence_neutrons = 1;
+        std::string interaction_filename = "../snt/w.snt";
+        const Interaction interaction = load_interaction(interaction_filename, n_valence_protons, n_valence_neutrons);
+        // create_hamiltonian(interaction);
+        H = create_hamiltonian_bit_representation(interaction);
+        timing.push_back(timer(start, "main").count());
+    }
+    print_vector(timing);
+
+    Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> es;
+    es.compute(H);
+    cout << "The eigenvalues of A are: " << es.eigenvalues().transpose() << endl;
+    
+    print("m_dim", H.rows());
+    // print("n_proton_orbitals", interaction.model_space_protons.orbitals.size());
+    // print("n_neutron_orbitals", interaction.model_space_neutrons.orbitals.size());
     // print("n_core_protons", interaction.n_core_protons);
     // print("n_core_neutrons", interaction.n_core_neutrons);
-
-    // print(interaction.model_space_protons.orbitals);
-    // print(interaction.model_space_neutrons.orbitals);
-    // print_vector(j_couple);
     return 0;
 }
