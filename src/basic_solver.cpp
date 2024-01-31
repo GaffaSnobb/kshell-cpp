7f42cadb3d39d4b2ae0e80c188f145730d30f8ad
GaffaSnobb <jon@dira.no>
2024-01-31 22:10:15 +0900

    Add test-run code

--- src/basic_solver.cpp
+++ src/basic_solver.cpp
@@ -8,6 +8,8 @@
 #include "loaders.hpp"
 #include "tools.hpp"
 #include "hamiltonian.hpp"
+#include "hamiltonian_device.hpp"
+#include "hamiltonian_bitset_representation.hpp"
 #include "../external/cppitertools/combinations.hpp"
 #include "../external/eigen-3.4.0/Eigen/Dense"
 #include "../external/eigen-3.4.0/Eigen/Eigenvalues"
@@ -21,72 +23,51 @@ using std::endl;
 
 int main(int argc, char* argv[])
 {
-
-    // std::bitset<16> state_0;
-    // state_0.set(14);
-    // cout << (not 0) << endl;
-    // cout << "state_0: " << state_0 << endl;
-    // state_0.reset(14);
-    // cout << "state_0: " << state_0 << endl;
-
-    // double a = 98.5;
-    // cout << a << endl;
-    // cout << a*state_0.test(14) << endl;
-
-    // cout << res << endl;
-    // state.set(3);
-    // if (state.test(4)) cout << "Sub-state 5 is occupied." << endl;
-    // state.reset(4);
-    // state.flip(9);
-
-    // std::hash<std::bitset<16>> hasher;
-    // size_t hash_value = hasher(state_0);
-
-    // cout << hash_value << endl;
-
+    // unsigned long long state = 0;
+    // print_bit_representation(state);
+    // state |= (1ULL << 63);
+    // print_bit_representation(state);
     // return 0;
 
     // unsigned short n_valence_protons = 1;
     // unsigned short n_valence_neutrons = 2;
     unsigned short n_valence_protons = std::stoi(argv[1]);
     unsigned short n_valence_neutrons = std::stoi(argv[2]);
-    
-    print("n_valence_protons", n_valence_protons);
-    print("n_valence_neutrons", n_valence_neutrons);
 
+    // Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> H_bitset;
     Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> H;
 
     std::vector<int> timing;
-    for (int i = 0; i < 1; i++)
+    for (int i = 0; i < 5; i++)
     {
         auto start = timer();
         std::string interaction_filename = "../snt/w.snt";
-        const Interaction interaction = load_interaction(interaction_filename, n_valence_protons, n_valence_neutrons);
-        // create_hamiltonian(interaction);
-        H = create_hamiltonian_bit_representation(interaction);
+        const Interaction interaction = loaders::load_interaction(interaction_filename, n_valence_protons, n_valence_neutrons);
+        // H_bitset = hamiltonian_bitset::create_hamiltonian_bitset_representation(interaction);
+        // H = hamiltonian::create_hamiltonian_primitive_bit_representation(interaction);
+        hamiltonian_device::create_hamiltonian_device_dispatcher(interaction);
         timing.push_back(timer(start, "main").count());
     }
+
+
     print_vector(timing);
+    cout << mean(timing) << endl;
+    // cout << (H == H_bitset) << endl;
 
-    auto start = timer();
-    Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> es;
-    es.compute(H);
-    es.eigenvalues();
-    timer(start, "eigensolver");
+    // auto start = timer();
+    // Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> es;
+    // es.compute(H);
+    // es.eigenvalues();
+    // timer(start, "eigensolver");
 
-    std::ofstream outfile("matrix.txt");
-    if (outfile.is_open())
-    {
-        outfile << H << std::endl;
-        outfile.close();
-    }
+    // std::ofstream outfile("matrix.txt");
+    // if (outfile.is_open())
+    // {
+    //     outfile << H << std::endl;
+    //     outfile.close();
+    // }
 
     // cout << "The eigenvalues of A are: " << es.eigenvalues().transpose() << endl;
     
-    // print("m_dim", H.rows());
-    // print("n_proton_orbitals", interaction.model_space_protons.orbitals.size());
-    // print("n_neutron_orbitals", interaction.model_space_neutrons.orbitals.size());
-    // print("n_core_protons", interaction.n_core_protons);
-    // print("n_core_neutrons", interaction.n_core_neutrons);
     return 0;
 }
