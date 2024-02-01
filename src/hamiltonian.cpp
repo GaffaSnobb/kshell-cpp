49408b907482882ea229c3d4b54277bbe4edb4c3
GaffaSnobb <jon@dira.no>
2024-02-01 17:29:29 +0900

    Set up a reference H calculation before modification to two-body calc

--- src/hamiltonian.cpp
+++ src/hamiltonian.cpp
@@ -292,6 +292,102 @@ double calculate_twobody_matrix_element_primitive_bit_representation(
     return twobody_res;
 }
 
+double calculate_twobody_matrix_element_primitive_bit_representation_new(
+    const Interaction& interaction,
+    const Indices& indices,
+    const unsigned long long& left_state,
+    const unsigned long long& right_state
+)
+{
+    double twobody_res = 0;
+    // double creation_res_switch = 0; // For removing if statements in the inner creation loop. Multiply the result by 0 instead of using if (condition) continue;.
+    const unsigned int n_indices = indices.creation_orb_indices_0.size();
+    for (unsigned int i = 0; i < n_indices; i++)
+    {
+        const unsigned short creation_orb_idx_0 = indices.creation_orb_indices_0[i];
+        const unsigned short creation_orb_idx_1 = indices.creation_orb_indices_1[i];
+        const unsigned short annihilation_orb_idx_0 = indices.annihilation_orb_indices_0[i];
+        const unsigned short annihilation_orb_idx_1 = indices.annihilation_orb_indices_1[i];
+        const unsigned short j_coupled = indices.j_coupled[i];
+        const short m_coupled = indices.m_coupled[i];
+        const double tbme = indices.tbme[i];
+
+        const double creation_norm = 1/std::sqrt(1 + (creation_orb_idx_0 == creation_orb_idx_1));
+        const double annihilation_norm = 1/std::sqrt(1 + (annihilation_orb_idx_0 == annihilation_orb_idx_1));
+
+        // Annihilation terms
+        for (unsigned short annihilation_comp_m_idx_0 : indices.orbital_idx_to_composite_m_idx_map[annihilation_orb_idx_0])
+        {
+            for (unsigned short annihilation_comp_m_idx_1 : indices.orbital_idx_to_composite_m_idx_map[annihilation_orb_idx_1])
+            {
+                // if (not right_state.test(annihilation_comp_m_idx_0))
+                if (not bittools::is_bit_set(right_state, annihilation_comp_m_idx_0)) continue;
+
+                unsigned long long new_right_state_annihilation = right_state;
+                const unsigned short n_operator_swaps_annihilation_0 = bittools::reset_bit_and_count_swaps(new_right_state_annihilation, annihilation_comp_m_idx_0);
+                short annihilation_sign = bittools::negative_one_pow(n_operator_swaps_annihilation_0);
+
+                // if (not new_right_state_annihilation.test(annihilation_comp_m_idx_1)) continue;
+                if (not bittools::is_bit_set(new_right_state_annihilation, annihilation_comp_m_idx_1)) continue;
+                const unsigned short n_operator_swaps_annihilation_1 = bittools::reset_bit_and_count_swaps(new_right_state_annihilation, annihilation_comp_m_idx_1);
+                annihilation_sign *= bittools::negative_one_pow(n_operator_swaps_annihilation_1);
+
+                const Key6 annihilation_key = {
+                    indices.orbital_idx_to_j_map[annihilation_orb_idx_0],
+                    indices.composite_m_idx_to_m_map[annihilation_comp_m_idx_0],
+                    indices.orbital_idx_to_j_map[annihilation_orb_idx_1],
+                    indices.composite_m_idx_to_m_map[annihilation_comp_m_idx_1],
+                    j_coupled,
+                    m_coupled
+                };
+
+                const double cg_annihilation = clebsch_gordan.at(annihilation_key);
+                if (cg_annihilation == 0) continue;
+
+                // Creation terms
+                double creation_res = 0.0;
+
+                for (unsigned short creation_comp_m_idx_0 : indices.orbital_idx_to_composite_m_idx_map[creation_orb_idx_0])
+                {
+                    for (unsigned short creation_comp_m_idx_1 : indices.orbital_idx_to_composite_m_idx_map[creation_orb_idx_1])
+                    {   
+                        if (bittools::is_bit_set(new_right_state_annihilation, creation_comp_m_idx_1)) continue;
+                        // creation_res_switch = (not new_right_state_annihilation[creation_comp_m_idx_1]);
+                        
+                        unsigned long long new_right_state_creation = new_right_state_annihilation;
+                        const unsigned short n_operator_swaps_creation_1 = bittools::set_bit_and_count_swaps(new_right_state_creation, creation_comp_m_idx_1);
+                        short creation_sign = bittools::negative_one_pow(n_operator_swaps_creation_1);
+
+                        if (bittools::is_bit_set(new_right_state_annihilation, creation_comp_m_idx_0)) continue;
+                        // creation_res_switch *= (not new_right_state_annihilation[creation_comp_m_idx_0]);
+                        
+                        const unsigned short n_operator_swaps_creation_0 = bittools::set_bit_and_count_swaps(new_right_state_creation, creation_comp_m_idx_0);
+                        creation_sign *= bittools::negative_one_pow(n_operator_swaps_creation_0);
+
+                        if (left_state != new_right_state_creation) continue;
+                        // creation_res_switch *= (left_state == new_right_state_creation);
+
+                        const Key6 creation_key = {
+                            indices.orbital_idx_to_j_map[creation_orb_idx_0],
+                            indices.composite_m_idx_to_m_map[creation_comp_m_idx_0],
+                            indices.orbital_idx_to_j_map[creation_orb_idx_1],
+                            indices.composite_m_idx_to_m_map[creation_comp_m_idx_1],
+                            j_coupled,
+                            m_coupled
+                        };
+
+                        const double cg_creation = clebsch_gordan.at(creation_key);
+                        // if (cg_creation == 0) continue;  // Might be faster to just multiply with 0 instead of checking.
+                        creation_res += creation_sign*cg_creation;//*creation_res_switch;
+                    }
+                }
+                twobody_res += annihilation_norm*creation_norm*tbme*creation_res*annihilation_sign*cg_annihilation;
+            }
+        }
+    }
+    return twobody_res;
+}
+
 void create_hamiltonian_primitive_bit_representation(const Interaction& interaction, const Indices& indices, double* H)
 {
     const unsigned int m_dim = interaction.basis_states.size();
@@ -314,46 +410,107 @@ void create_hamiltonian_primitive_bit_representation(const Interaction& interact
     }
     timer(start, "[HOST] one-body calc time");
 
-    // timer(start, "calculate_onebody_matrix_element_bit_representation");
-    // start = timer();
-    // int thread_id = omp_get_thread_num();
-    // std::vector<long long> loop_timings;
-    // auto loop_timer = timer();
+    start = timer();
+    int thread_id = omp_get_thread_num();
+    std::vector<long long> loop_timings;
+    auto loop_timer = timer();
 
-    // // #pragma omp parallel for //num_threads(6)
-    // // for (int left_idx : tq::trange(m_dim))
-    // for (unsigned int left_idx = 0; left_idx < m_dim; left_idx++)
-    // {   
-    //     if (thread_id == 0) loop_timer = timer();
+    // #pragma omp parallel for //num_threads(6)
+    // for (int left_idx : tq::trange(m_dim))
+    for (unsigned int row_idx = 0; row_idx < m_dim; row_idx++)
+    {   
+        if (thread_id == 0) loop_timer = timer();
         
-    //     #pragma omp parallel for
-    //     for (int right_idx = left_idx; right_idx < m_dim; right_idx++)
-    //     {    
-    //         H(left_idx, right_idx) += calculate_twobody_matrix_element_primitive_bit_representation(
-    //             interaction,
-    //             indices,
-    //             interaction.basis_states[left_idx],
-    //             interaction.basis_states[right_idx]
-    //         );
-    //     }
-    //     if (thread_id == 0)
-    //     {
-    //         loop_timings.push_back(timer(loop_timer));
-    //         double mean_time = mean(loop_timings)/1000;
-    //         int num_threads = omp_get_num_threads();
+        #pragma omp parallel for
+        for (int col_idx = row_idx; col_idx < m_dim; col_idx++)
+        {    
+            H[row_idx*m_dim + col_idx] += calculate_twobody_matrix_element_primitive_bit_representation(
+                interaction,
+                indices,
+                interaction.basis_states[row_idx],
+                interaction.basis_states[col_idx]
+            );
+        }
+        if (thread_id == 0)
+        {
+            loop_timings.push_back(timer(loop_timer));
+            double mean_time = mean(loop_timings)/1000;
+            int num_threads = omp_get_num_threads();
             
-    //         cout << "\r[" << left_idx << " of ≈ " << (double)m_dim/num_threads << "]";
-    //         cout << " [loop time: ";
-    //         cout << std::setfill(' ') << std::setw(5) << loop_timings.back()/1000. << " s";
-    //         cout << " - mean loop time: ";
-    //         cout << std::setfill(' ') << std::setw(10) << mean_time << " s]";
-    //         cout << " [est. time left: ";
-    //         cout << std::setfill(' ') << std::setw(10) << (m_dim - (left_idx + 1))*mean_time << " s]" << std::flush;
-    //     }
-    // }
-    // cout << endl;   // For the progress bar.
-    // timer(start, "calculate_twobody_matrix_element_bit_representation");
-    // complete_hermitian_matrix(H);    
-    // return H;
+            cout << "\r[" << row_idx << " of ≈ " << (double)m_dim/num_threads << "]";
+            cout << " [loop time: ";
+            cout << std::setfill(' ') << std::setw(5) << loop_timings.back()/1000. << " s";
+            cout << " - mean loop time: ";
+            cout << std::setfill(' ') << std::setw(10) << mean_time << " s]";
+            cout << " [est. time left: ";
+            cout << std::setfill(' ') << std::setw(10) << (m_dim - (row_idx + 1))*mean_time << " s]" << std::flush;
+        }
+    }
+    cout << endl;   // For the progress bar.
+    timer(start, "[HOST][REFERENCE] two-body calc time");
+    // complete_hermitian_matrix(H);
+}
+
+void create_hamiltonian_primitive_bit_representation_tmp(const Interaction& interaction, const Indices& indices, double* H)
+{
+    const unsigned int m_dim = interaction.basis_states.size();
+
+    auto start = timer();
+    #pragma omp parallel for
+    for (unsigned int idx = 0; idx < m_dim; idx++)
+    {
+        /*
+        As long as the single-particle energies are defined only when
+        \alpha = \beta, then we only need to calculate the diagonal
+        elements.
+        */
+        H[idx*m_dim + idx] = calculate_onebody_matrix_element_primitive_bit_representation(
+            interaction,
+            indices,
+            interaction.basis_states[idx],
+            interaction.basis_states[idx]
+        );
+    }
+    timer(start, "[HOST] one-body calc time");
+
+    start = timer();
+    int thread_id = omp_get_thread_num();
+    std::vector<long long> loop_timings;
+    auto loop_timer = timer();
+
+    // #pragma omp parallel for //num_threads(6)
+    // for (int left_idx : tq::trange(m_dim))
+    for (unsigned int row_idx = 0; row_idx < m_dim; row_idx++)
+    {   
+        if (thread_id == 0) loop_timer = timer();
+        
+        #pragma omp parallel for
+        for (int col_idx = row_idx; col_idx < m_dim; col_idx++)
+        {    
+            H[row_idx*m_dim + col_idx] += calculate_twobody_matrix_element_primitive_bit_representation(
+                interaction,
+                indices,
+                interaction.basis_states[row_idx],
+                interaction.basis_states[col_idx]
+            );
+        }
+        if (thread_id == 0)
+        {
+            loop_timings.push_back(timer(loop_timer));
+            double mean_time = mean(loop_timings)/1000;
+            int num_threads = omp_get_num_threads();
+            
+            cout << "\r[" << row_idx << " of ≈ " << (double)m_dim/num_threads << "]";
+            cout << " [loop time: ";
+            cout << std::setfill(' ') << std::setw(5) << loop_timings.back()/1000. << " s";
+            cout << " - mean loop time: ";
+            cout << std::setfill(' ') << std::setw(10) << mean_time << " s]";
+            cout << " [est. time left: ";
+            cout << std::setfill(' ') << std::setw(10) << (m_dim - (row_idx + 1))*mean_time << " s]" << std::flush;
+        }
+    }
+    cout << endl;   // For the progress bar.
+    timer(start, "[HOST][NEW] two-body calc time");
+    // complete_hermitian_matrix(H);
 }
 }
--- src/hamiltonian.hpp
+++ src/hamiltonian.hpp
@@ -6,5 +6,6 @@
 namespace hamiltonian
 {
 void create_hamiltonian_primitive_bit_representation(const Interaction& interaction, const Indices& indices, double* H);
+void create_hamiltonian_primitive_bit_representation_tmp(const Interaction& interaction, const Indices& indices, double* H);
 }
 #endif
