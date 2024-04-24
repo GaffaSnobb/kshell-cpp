709256d246556378fafafa8a6cc546e682edd352
GaffaSnobb <jon@dira.no>
2024-04-24 23:02:50 +0900

    Add sparse CG array calculator

--- scripts/clebsch_gordan_generator.py
+++ scripts/clebsch_gordan_generator.py
@@ -1,7 +1,7 @@
 from sympy.physics.quantum.cg import CG
 import numpy as np
 
-def main():
+def generate_cg_array():
     arr = np.zeros(shape=(3, 6, 3, 6, 6))
     # arr = np.zeros(shape=(3, 6), dtype=tuple)
     arr_flat = np.zeros(1944)
@@ -47,8 +47,8 @@ def main():
                         )
 
                         calculated_flat_idx = ((((j1_idx*6 + m1_idx)*3 + j2_idx)*6 + m2_idx)*6 + j3_idx)    # Indexing a 1D array as if it was a 5D array.
-                        arr[j1_idx, m1_idx, j2_idx, m2_idx, j3_idx] = float(cg.doit())
-                        arr_flat[calculated_flat_idx] = float(cg.doit())
+                        # arr[j1_idx, m1_idx, j2_idx, m2_idx, j3_idx] = float(cg.doit())
+                        # arr_flat[calculated_flat_idx] = float(cg.doit())
 
                         # if (j1_idx == 1) and (m1_idx == 0) and (j2_idx == 2) and (m2_idx == 4) and (j3_idx == 2):
                         #     print(f"{j1_idx = }")
@@ -76,6 +76,63 @@ def main():
             outfile.write(f"{arr_flat[i]:21.16f}" + msg)
 
     print(len(arr_flat[arr_flat == 0]))
+    print()
+
+def generate_cg_array_sparse():
+    arr_flat = []
+    m_vals = {
+        1: [-1, 1],
+        3: [-3, -1, 1, 3],
+        5: [-5, -3, -1, 1, 3, 5],
+    }
+    j_vals = [1, 3, 5]
+
+    for j1 in j_vals:
+        for m1 in m_vals[j1]:
+
+            for j2 in j_vals:
+                for m2 in m_vals[j2]:
+                    m3 = m1 + m2
+
+                    j3_vals = range(abs( j1 - j2 ), j1 + j2 + 2, 2)
+                    for j3 in j3_vals:
+
+                        j1_idx = (j1 + 1)//2 - 1        # [1, 3, 5] -> [0, 1, 2]
+                        m1_idx = (m1 + j1)//2           # [-5, -3, -1, 1, 3, 5] -> [0, 1, 2, 3, 4, 5], [-1, 1] -> [0, 1]
+                        j2_idx = (j2 + 1)//2 - 1
+                        m2_idx = (m2 + j2)//2
+                        j3_idx = (j3 - j3_vals[0])//2   # [0,  2,  4,  6,  8, 10] -> [0, 1, 2, 3, 4, 5], [2, 4, 6, 8] -> [0, 1, 2, 3]
+
+                        cg = CG(
+                            j1 = j1/2,
+                            m1 = m1/2,
+                            j2 = j2/2,
+                            m2 = m2/2,
+                            j3 = j3/2,
+                            m3 = m3/2,
+                        )
+
+                        calculated_flat_idx = ((((j1_idx*len(m_vals[j1]) + m1_idx)*3 + j2_idx)*6 + len(m_vals[j2]))*len(j3_vals) + j3_idx)    # Indexing a 1D array as if it was a 5D array.
+                        arr_flat.append(float(cg.doit()))
+
+    arr_flat = np.array(arr_flat)
+    print(f"{arr_flat.ndim = }")
+    print(f"{arr_flat.size = }")
+    print(f"{arr_flat.nbytes = }")
+    print(len(arr_flat[arr_flat == 0]))
+
+    msg = ", "
+    with open("cg_flat_sparse_arr.txt", "w") as outfile:
+        for i in range(len(arr_flat)):
+
+            if (i + 1)%3 == 0:
+                msg = ",\n"
+            else:
+                msg = ", "
+
+            outfile.write(f"{arr_flat[i]:21.16f}" + msg)
+
 
 if __name__ == "__main__":
-    main()
+    # generate_cg_array()
+    generate_cg_array_sparse()
