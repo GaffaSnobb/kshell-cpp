39ae757ead0440832556a4f570fb5bdcc53ba5b2
GaffaSnobb <jon@dira.no>
2024-04-25 23:41:11 +0900

    Change to stdint.h int types

--- src/bit_manipulation_tools_device.hpp
+++ src/bit_manipulation_tools_device.hpp
@@ -1,19 +1,20 @@
 #ifndef BIT_MANIPULATION_TOOLS_DEVICE_HPP
 #define BIT_MANIPULATION_TOOLS_DEVICE_HPP
 
+#include <stdint.h>
 #include <hip/hip_runtime.h>
 
 namespace bittools_device
 {
-__device__ inline unsigned short reset_bit_and_count_swaps(unsigned long long& state, const unsigned short bit_to_reset)
+__device__ inline uint16_t reset_bit_and_count_swaps(uint64_t& state, const uint16_t bit_to_reset)
 {
-    const unsigned long long mask = (1ULL << bit_to_reset) - 1; // Create a mask for all bits before the bit_to_reset.
-    const unsigned int count = __popcll(state & mask);          // Count set bits before bit_to_set using popcll.
-    state &= ~(1ULL << bit_to_reset);                           // Reset the bit at bit_to_reset.
+    const uint64_t mask = (1ULL << bit_to_reset) - 1;   // Create a mask for all bits before the bit_to_reset.
+    const uint32_t count = __popcll(state & mask);      // Count set bits before bit_to_set using popcll.
+    state &= ~(1ULL << bit_to_reset);                   // Reset the bit at bit_to_reset.
     return count;
 }
 
-__device__ inline bool is_bit_set(unsigned long long state, const unsigned short bit_to_check)
+__device__ inline bool is_bit_set(uint64_t state, const uint16_t bit_to_check)
 {
     /*
     Check if a specific bit is set.
@@ -21,15 +22,15 @@ __device__ inline bool is_bit_set(unsigned long long state, const unsigned short
     return (state & (1ULL << bit_to_check)) != 0;
 }
 
-__device__ inline unsigned short set_bit_and_count_swaps(unsigned long long& state, const unsigned short bit_to_set)
+__device__ inline uint16_t set_bit_and_count_swaps(uint64_t& state, const uint16_t bit_to_set)
 {
-    const unsigned long long mask = (1ULL << bit_to_set) - 1; // Create a mask for bits before bit_to_set.
-    const unsigned int count = __popcll(state & mask);        // Count set bits before bit_to_set using popcll.
-    state |= (1ULL << bit_to_set);                            // Set the bit at bit_to_set.
+    const uint64_t mask = (1ULL << bit_to_set) - 1; // Create a mask for bits before bit_to_set.
+    const uint32_t count = __popcll(state & mask);  // Count set bits before bit_to_set using popcll.
+    state |= (1ULL << bit_to_set);                  // Set the bit at bit_to_set.
     return count;
 }
 
-__device__ inline short negative_one_pow(const short exponent)
+__device__ inline int16_t negative_one_pow(const int16_t exponent)
 {
     /*
     Calculates -1 raised to the power of `exponent` efficiently using
--- src/hamiltonian_device.cpp
+++ src/hamiltonian_device.cpp
@@ -1,6 +1,8 @@
 #include <vector>
 #include <iostream>
+#include <stdint.h>
 #include <hip/hip_runtime.h>
+
 #include "hip_wrappers.hpp"
 #include "bit_manipulation_tools_device.hpp"
 #include "data_structures.hpp"
@@ -14,38 +16,40 @@ using std::endl;
 namespace hamiltonian_device
 {
 __device__ double calculate_onebody_matrix_element(
-    const unsigned short n_orbitals,
+    const uint16_t n_orbitals,
     const double* spe,
-    const unsigned short* orbital_idx_to_composite_m_idx_map_flattened_indices,
-    const unsigned long long& left_state,
-    const unsigned long long& right_state
+    const uint16_t* orbital_idx_to_composite_m_idx_map_flattened_indices,
+    const uint64_t& left_state,
+    const uint64_t& right_state
 )
 {
     double onebody_res = 0;
-    unsigned short creation_start_m_idx = 0;
-    unsigned short annihilation_start_m_idx = 0;
+    uint16_t creation_start_m_idx = 0;
+    uint16_t annihilation_start_m_idx = 0;
     
-    for (unsigned short creation_and_annihilation_orb_idx = 0; creation_and_annihilation_orb_idx < n_orbitals; creation_and_annihilation_orb_idx++)
+    for (uint16_t creation_and_annihilation_orb_idx = 0; creation_and_annihilation_orb_idx < n_orbitals; creation_and_annihilation_orb_idx++)
     {
-        const unsigned short creation_end_m_idx = orbital_idx_to_composite_m_idx_map_flattened_indices[creation_and_annihilation_orb_idx];
-        const unsigned short annihilation_end_m_idx = orbital_idx_to_composite_m_idx_map_flattened_indices[creation_and_annihilation_orb_idx];
+        const uint16_t creation_end_m_idx =
+            orbital_idx_to_composite_m_idx_map_flattened_indices[creation_and_annihilation_orb_idx];
+        const uint16_t annihilation_end_m_idx =
+            orbital_idx_to_composite_m_idx_map_flattened_indices[creation_and_annihilation_orb_idx];
 
-        for (unsigned short creation_comp_m_idx = creation_start_m_idx; creation_comp_m_idx < creation_end_m_idx; creation_comp_m_idx++)
+        for (uint16_t creation_comp_m_idx = creation_start_m_idx; creation_comp_m_idx < creation_end_m_idx; creation_comp_m_idx++)
         {
-            for (unsigned short annihilation_comp_m_idx = annihilation_start_m_idx; annihilation_comp_m_idx < annihilation_end_m_idx; annihilation_comp_m_idx++)
+            for (uint16_t annihilation_comp_m_idx = annihilation_start_m_idx; annihilation_comp_m_idx < annihilation_end_m_idx; annihilation_comp_m_idx++)
             {
                 // double switch_ = 1; // To eliminate if-statements.
-                unsigned long long new_right_state = right_state;   // The contents of right_state is copied, not referenced.
+                uint64_t new_right_state = right_state;   // The contents of right_state is copied, not referenced.
 
                 // switch_ = switch_*bittools_device::is_bit_set(new_right_state, annihilation_comp_m_idx);
                 if (not bittools_device::is_bit_set(new_right_state, annihilation_comp_m_idx)) continue;
-                const unsigned short n_operator_swaps_annihilation = bittools_device::reset_bit_and_count_swaps(new_right_state, annihilation_comp_m_idx);
-                const short annihilation_sign = bittools_device::negative_one_pow(n_operator_swaps_annihilation);
+                const uint16_t n_operator_swaps_annihilation = bittools_device::reset_bit_and_count_swaps(new_right_state, annihilation_comp_m_idx);
+                const int16_t annihilation_sign = bittools_device::negative_one_pow(n_operator_swaps_annihilation);
 
                 // switch_ = switch_*(not bittools_device::is_bit_set(new_right_state, creation_comp_m_idx));
                 if (bittools_device::is_bit_set(new_right_state, creation_comp_m_idx)) continue;
-                const unsigned short n_operator_swaps_creation = bittools_device::set_bit_and_count_swaps(new_right_state, creation_comp_m_idx);
-                const short creation_sign = bittools_device::negative_one_pow(n_operator_swaps_creation);
+                const uint16_t n_operator_swaps_creation = bittools_device::set_bit_and_count_swaps(new_right_state, creation_comp_m_idx);
+                const int16_t creation_sign = bittools_device::negative_one_pow(n_operator_swaps_creation);
 
                 // switch_ = switch_*(left_state == new_right_state);
                 if (left_state != new_right_state) continue;
@@ -60,22 +64,22 @@ __device__ double calculate_onebody_matrix_element(
 
 __global__ void onebody_matrix_element_dispatcher(
     double* H_diag,
-    const unsigned long long* basis_states,
+    const uint64_t* basis_states,
     const double* spe,
-    const unsigned short* orbital_idx_to_composite_m_idx_map_flattened_indices,
-    const unsigned int m_dim,
-    const unsigned short n_orbitals
+    const uint16_t* orbital_idx_to_composite_m_idx_map_flattened_indices,
+    const uint32_t m_dim,
+    const uint16_t n_orbitals
 )
 {
-    int idx = blockIdx.x*blockDim.x + threadIdx.x;
-    // int row_idx = idx/m_dim;
-    // int col_idx = idx%m_dim;
+    const int32_t idx = blockIdx.x*blockDim.x + threadIdx.x;
+    // int32_t row_idx = idx/m_dim;
+    // int32_t col_idx = idx%m_dim;
 
     // if ((row_idx < m_dim) and (col_idx < m_dim) and (row_idx == col_idx))
     if (idx < m_dim)
     {
-        const unsigned long long left_state = basis_states[idx];
-        const unsigned long long right_state = basis_states[idx];
+        const uint64_t left_state = basis_states[idx];
+        const uint64_t right_state = basis_states[idx];
         
         H_diag[idx] = calculate_onebody_matrix_element(
             n_orbitals,
@@ -87,24 +91,146 @@ __global__ void onebody_matrix_element_dispatcher(
     }
 }
 
-__global__ void twobody_matrix_element_dispatcher(
-    double* H_diag,
-    const unsigned long long* basis_states,
-    const double* spe,
-    const unsigned short* orbital_idx_to_composite_m_idx_map_flattened_indices,
-    const unsigned int m_dim,
-    const unsigned short n_orbitals
+__device__ double calculate_twobody_matrix_element(
+    const Indices& indices,
+    const uint64_t& left_state,
+    const uint64_t& right_state
 )
 {
-    int idx = blockIdx.x*blockDim.x + threadIdx.x;
-    // int row_idx = idx/m_dim;
-    // int col_idx = idx%m_dim;
+    uint16_t j1_idx, m1_idx, j2_idx, m2_idx, j3_idx;  // Indices for the Clebsch-Gordan 5D array...
+    uint16_t flat_cg_idx;                             // ...translated to a flattened 1D array.
+
+    double twobody_res = 0;
+    // double creation_res_switch = 0; // For removing if statements in the inner creation loop. Multiply the result by 0 instead of using if (condition) continue;.
+    const uint32_t n_indices = indices.creation_orb_indices_0.size();
+    for (size_t i = 0; i < n_indices; i++)
+    {
+        const uint16_t creation_orb_idx_0 = indices.creation_orb_indices_0[i];
+        const uint16_t creation_orb_idx_1 = indices.creation_orb_indices_1[i];
+        const uint16_t annihilation_orb_idx_0 = indices.annihilation_orb_indices_0[i];
+        const uint16_t annihilation_orb_idx_1 = indices.annihilation_orb_indices_1[i];
+        const uint16_t j_coupled = indices.j_coupled[i];
+        const int16_t m_coupled = indices.m_coupled[i];
+        const double tbme = indices.tbme[i];
+
+        const double creation_norm = inverse_sqrt_2[creation_orb_idx_0 == creation_orb_idx_1];
+        const double annihilation_norm = inverse_sqrt_2[annihilation_orb_idx_0 == annihilation_orb_idx_1];
+
+        // Annihilation terms
+        for (uint16_t annihilation_comp_m_idx_0 : indices.orbital_idx_to_composite_m_idx_map[annihilation_orb_idx_0])
+        {
+            for (uint16_t annihilation_comp_m_idx_1 : indices.orbital_idx_to_composite_m_idx_map[annihilation_orb_idx_1])
+            {
+                if (
+                    (indices.composite_m_idx_to_m_map[annihilation_comp_m_idx_0] +  // m1
+                    indices.composite_m_idx_to_m_map[annihilation_comp_m_idx_1]) != // m2
+                    m_coupled                                                       // M
+                )
+                {
+                    /*
+                    The Clebsch-Gordan coefficient is always zero when
+                    m1 + m2 != M. This is because of conservation of the
+                    z-component of angular momentum in the coupling
+                    process.
+                    */
+                    continue;
+                }
+                
+                // if (not right_state.test(annihilation_comp_m_idx_0))
+                if (not bittools_device::is_bit_set(right_state, annihilation_comp_m_idx_0)) continue;
+
+                uint64_t new_right_state_annihilation = right_state;
+                const uint16_t n_operator_swaps_annihilation_0 = bittools_device::reset_bit_and_count_swaps(new_right_state_annihilation, annihilation_comp_m_idx_0);
+                int16_t annihilation_sign = bittools_device::negative_one_pow(n_operator_swaps_annihilation_0);
+
+                // if (not new_right_state_annihilation.test(annihilation_comp_m_idx_1)) continue;
+                if (not bittools_device::is_bit_set(new_right_state_annihilation, annihilation_comp_m_idx_1)) continue;
+                const uint16_t n_operator_swaps_annihilation_1 = bittools_device::reset_bit_and_count_swaps(new_right_state_annihilation, annihilation_comp_m_idx_1);
+                annihilation_sign *= bittools_device::negative_one_pow(n_operator_swaps_annihilation_1);
+                
+                j1_idx = (indices.orbital_idx_to_j_map[annihilation_orb_idx_0] + 1)/2 - 1;
+                m1_idx = (indices.composite_m_idx_to_m_map[annihilation_comp_m_idx_0] + 5)/2;
+                j2_idx = (indices.orbital_idx_to_j_map[annihilation_orb_idx_1] + 1)/2 - 1;
+                m2_idx = (indices.composite_m_idx_to_m_map[annihilation_comp_m_idx_1] + 5)/2;
+                j3_idx = j_coupled/2;
+                flat_cg_idx = ((((j1_idx*6 + m1_idx)*3 + j2_idx)*6 + m2_idx)*6 + j3_idx);    // Indexing a 1D array as if it was a 5D array.
+
+                const double cg_annihilation = clebsch_gordan_array[flat_cg_idx];
+
+                // Creation terms
+                double creation_res = 0.0;
+
+                for (uint16_t creation_comp_m_idx_0 : indices.orbital_idx_to_composite_m_idx_map[creation_orb_idx_0])
+                {
+                    for (uint16_t creation_comp_m_idx_1 : indices.orbital_idx_to_composite_m_idx_map[creation_orb_idx_1])
+                    {   
+                        if (
+                            (indices.composite_m_idx_to_m_map[creation_comp_m_idx_0] +  // m1
+                            indices.composite_m_idx_to_m_map[creation_comp_m_idx_1]) != // m2
+                            m_coupled                                                   // M
+                        )
+                        {
+                            /*
+                            The Clebsch-Gordan coefficient is always zero when
+                            m1 + m2 != M. This is because of conservation of the
+                            z-component of angular momentum in the coupling
+                            process.
+                            */
+                            continue;
+                        }
+                        if (bittools_device::is_bit_set(new_right_state_annihilation, creation_comp_m_idx_1)) continue;
+                        // creation_res_switch = (not new_right_state_annihilation[creation_comp_m_idx_1]);
+                        
+                        uint64_t new_right_state_creation = new_right_state_annihilation;
+                        const uint16_t n_operator_swaps_creation_1 = bittools_device::set_bit_and_count_swaps(new_right_state_creation, creation_comp_m_idx_1);
+                        int16_t creation_sign = bittools_device::negative_one_pow(n_operator_swaps_creation_1);
+
+                        if (bittools_device::is_bit_set(new_right_state_annihilation, creation_comp_m_idx_0)) continue;
+                        // creation_res_switch *= (not new_right_state_annihilation[creation_comp_m_idx_0]);
+                        
+                        const uint16_t n_operator_swaps_creation_0 = bittools_device::set_bit_and_count_swaps(new_right_state_creation, creation_comp_m_idx_0);
+                        creation_sign *= bittools_device::negative_one_pow(n_operator_swaps_creation_0);
+
+                        if (left_state != new_right_state_creation) continue;
+                        // creation_res_switch *= (left_state == new_right_state_creation);
+
+                        j1_idx = (indices.orbital_idx_to_j_map[creation_orb_idx_0] + 1)/2 - 1;
+                        m1_idx = (indices.composite_m_idx_to_m_map[creation_comp_m_idx_0] + 5)/2;
+                        j2_idx = (indices.orbital_idx_to_j_map[creation_orb_idx_1] + 1)/2 - 1;
+                        m2_idx = (indices.composite_m_idx_to_m_map[creation_comp_m_idx_1] + 5)/2;
+                        flat_cg_idx = ((((j1_idx*6 + m1_idx)*3 + j2_idx)*6 + m2_idx)*6 + j3_idx);    // Indexing a 1D array as if it was a 5D array.
+
+                        const double cg_creation = clebsch_gordan_array[flat_cg_idx];
+
+                        // if (cg_creation == 0) continue;  // Might be faster to just multiply with 0 instead of checking.
+                        creation_res += creation_sign*cg_creation;//*creation_res_switch;
+                    }
+                }
+                twobody_res += annihilation_norm*creation_norm*tbme*creation_res*annihilation_sign*cg_annihilation;
+            }
+        }
+    }
+    return twobody_res;
+}
+
+__global__ void twobody_matrix_element_dispatcher(
+    double* H_diag,
+    const uint64_t* basis_states,
+    const double* spe,
+    const uint16_t* orbital_idx_to_composite_m_idx_map_flattened_indices,
+    const uint32_t m_dim,
+    const uint16_t n_orbitals
+)
+{
+    int32_t idx = blockIdx.x*blockDim.x + threadIdx.x;
+    // int32_t row_idx = idx/m_dim;
+    // int32_t col_idx = idx%m_dim;
 
     // if ((row_idx < m_dim) and (col_idx < m_dim) and (row_idx == col_idx))
     if (idx < m_dim)
     {
-        const unsigned long long left_state = basis_states[idx];
-        const unsigned long long right_state = basis_states[idx];
+        const uint64_t left_state = basis_states[idx];
+        const uint64_t right_state = basis_states[idx];
         
         H_diag[idx] = calculate_onebody_matrix_element(
             n_orbitals,
@@ -118,26 +244,26 @@ __global__ void twobody_matrix_element_dispatcher(
 
 void create_hamiltonian_device_dispatcher(const Interaction& interaction, const Indices& indices, double* H)
 {
-    const unsigned int m_dim = interaction.basis_states.size();
-    const unsigned short n_orbitals = interaction.model_space.n_orbitals;
+    const uint32_t m_dim = interaction.basis_states.size();
+    const uint16_t n_orbitals = interaction.model_space.n_orbitals;
 
-    const int threads_per_block = 128;
-    const int blocks_per_grid = (m_dim + threads_per_block - 1) / threads_per_block;
+    const int32_t threads_per_block = 128;
+    const int32_t blocks_per_grid = (m_dim + threads_per_block - 1) / threads_per_block;
     double* H_diag_device = nullptr;
-    unsigned long long* basis_states_device = nullptr;
+    uint64_t* basis_states_device = nullptr;
     double* spe_array_device = nullptr;
-    unsigned short* orbital_idx_to_composite_m_idx_map_flattened_indices_device = nullptr;
+    uint16_t* orbital_idx_to_composite_m_idx_map_flattened_indices_device = nullptr;
+    double* H_diag_tmp = new double[m_dim];
     
     auto start = timer();
-        double* H_diag_tmp = new double[m_dim];
         hip_wrappers::hipMalloc(&H_diag_device, m_dim*sizeof(double));
-        hip_wrappers::hipMalloc(&basis_states_device, m_dim*sizeof(unsigned long long));
+        hip_wrappers::hipMalloc(&basis_states_device, m_dim*sizeof(uint64_t));
         hip_wrappers::hipMalloc(&spe_array_device, n_orbitals*sizeof(double));
-        hip_wrappers::hipMalloc(&orbital_idx_to_composite_m_idx_map_flattened_indices_device, n_orbitals*sizeof(unsigned short));
+        hip_wrappers::hipMalloc(&orbital_idx_to_composite_m_idx_map_flattened_indices_device, n_orbitals*sizeof(uint16_t));
         
-        hip_wrappers::hipMemcpy(basis_states_device, interaction.basis_states.data(), m_dim*sizeof(unsigned long long), hipMemcpyHostToDevice);
+        hip_wrappers::hipMemcpy(basis_states_device, interaction.basis_states.data(), m_dim*sizeof(uint64_t), hipMemcpyHostToDevice);
         hip_wrappers::hipMemcpy(spe_array_device, interaction.spe_array, n_orbitals*sizeof(double), hipMemcpyHostToDevice);
-        hip_wrappers::hipMemcpy(orbital_idx_to_composite_m_idx_map_flattened_indices_device, indices.orbital_idx_to_composite_m_idx_map_flattened_indices, n_orbitals*sizeof(unsigned short), hipMemcpyHostToDevice);
+        hip_wrappers::hipMemcpy(orbital_idx_to_composite_m_idx_map_flattened_indices_device, indices.orbital_idx_to_composite_m_idx_map_flattened_indices, n_orbitals*sizeof(uint16_t), hipMemcpyHostToDevice);
 
         hipLaunchKernelGGL(
             onebody_matrix_element_dispatcher, dim3(blocks_per_grid), dim3(threads_per_block), 0, 0,
@@ -157,7 +283,7 @@ void create_hamiltonian_device_dispatcher(const Interaction& interaction, const 
         hip_wrappers::hipFree(orbital_idx_to_composite_m_idx_map_flattened_indices_device);
     timer(start, "[DEVICE] one-body calc, alloc, copy, and free time");
 
-    for (int diag_idx = 0; diag_idx < m_dim; diag_idx++) H[diag_idx*m_dim + diag_idx] = H_diag_tmp[diag_idx]; // Copy data to the diagonal of H.
+    for (size_t diag_idx = 0; diag_idx < m_dim; diag_idx++) H[diag_idx*m_dim + diag_idx] = H_diag_tmp[diag_idx]; // Copy data to the diagonal of H.
     delete[] H_diag_tmp;
 }
 }
