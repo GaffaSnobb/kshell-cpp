c36398ba82b4080562bf598f72d452508e5666c5
GaffaSnobb <jon@dira.no>
2024-06-19 23:04:25 +0900

    Add docstrings

--- src/linear_algebra.cpp
+++ src/linear_algebra.cpp
@@ -6,7 +6,12 @@
 namespace lalg
 {
 void mat_vec_mul(const double* mat, const double* vec, const size_t m_dim, double* res)
-{
+{   /*
+    Left multiply a (m_dim x m_dim) matrix on a vector, save the
+    resulting vector in `res`:
+
+        res = mat@vec
+    */
     for (size_t row_idx = 0; row_idx < m_dim; row_idx++)
     {
         res[row_idx] = 0;
@@ -17,19 +22,26 @@ void mat_vec_mul(const double* mat, const double* vec, const size_t m_dim, doubl
     }
 }
 double vec_vec_dot(const double *vec_0, const double *vec_1, const size_t size)
-{
+{   /*
+    Take the dot product of `vec_0` and `vec_1`.
+    */
     double sum = 0;
     for (size_t i = 0; i < size; i++) sum += vec_0[i]*vec_1[i];
     return sum;
 }
 void normalise_vector(double *vec, const size_t size)
-{
+{   /*
+    Normalise `vec`, aka. make its length / norm / magnitude equal to 1.
+    */
     const double norm = std::sqrt(vec_vec_dot(vec, vec, size));
     if (norm <= 0) throw std::runtime_error("Vector norm has to be larger than 0!");
     for (size_t i = 0; i < size; i++) vec[i] /= norm;
 }
 void vec_div(const double *vec, const double factor, const size_t size, double *res)
-{
+{   /*
+    Divide every element of `vec` by `factor`. Store the resulting
+    vector in `res`.
+    */
     for (size_t i = 0; i < size; i++) res[i] = vec[i]/factor;
 }
 } // namespace lalg
